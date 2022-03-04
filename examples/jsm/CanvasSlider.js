0e904d3d59744c1515fcaddcd7301fafd536faf4
Nicholas Lever <nik@catalystpics.co.uk>
2022-03-04 12:09:17 +0000

    Working on Slider

--- examples/jsm/CanvasSlider.js
+++ examples/jsm/CanvasSlider.js
@@ -10,9 +10,11 @@ class CanvasSlider{
     init(x, y, w, h){
         this.left = w*0.85;
         let gap = Math.max(w*0.05, h*0.05);
-        this.slider = {x,y,width:left-gap,height:h};
-        this.text = {x:left + x,y,width:w-left,height:h};
+        this.slider = {x,y,width:this.left-gap,height:h};
+        this.text = {x:this.left + x,y,width:w-this.left,height:h};
         const fontSize = this.text.width * 0.2;
+        this.middle = h/2;
+        this.x = x;
         this.font = `${fontSize}px sans-serif`;
     }
 
@@ -25,6 +27,8 @@ class CanvasSlider{
         ctx.font = this.font;
         ctx.textAlign = 'left';
         ctx.textBaseline = 'middle';
-        ctx.fillText(this.value.toFixed(2));
+        ctx.fillText(this.value.toFixed(2), this.x, this.middle);
     }
 }
+
+export { CanvasSlider };
