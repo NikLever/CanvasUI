ec83594d5deb5ebd246d10faa9628727978dcc8f
Nicholas Lever <nik@catalystpics.co.uk>
2022-03-04 11:07:25 +0000

    Refactored CanvasSlider

---
+++ examples/jsm/CanvasSlider.js
@@ -0,0 +1,30 @@
+class CanvasSlider{
+    constructor(x, y, w, h, range={min:0, max:1}, value=0.5){
+        this.min = range.min;
+        this.max = range.max;
+        this.value = value;
+        this.init(x, y, w, h);
+        this.needsUpdate = false;
+    }
+  
+    init(x, y, w, h){
+        this.left = w*0.85;
+        let gap = Math.max(w*0.05, h*0.05);
+        this.slider = {x,y,width:left-gap,height:h};
+        this.text = {x:left + x,y,width:w-left,height:h};
+        const fontSize = this.text.width * 0.2;
+        this.font = `${fontSize}px sans-serif`;
+    }
+
+    drawSlider(ctx){
+
+    }
+
+    update(ctx){
+        this.drawSlider(ctx)
+        ctx.font = this.font;
+        ctx.textAlign = 'left';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(this.value.toFixed(2));
+    }
+}
